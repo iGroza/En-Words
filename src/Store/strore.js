a5ca4e03f2c15a637e51da7f5e03ee4adfc696d5
iGroza <xneonxneon1@gmail.com>
2020-01-29 02:06:35 +0700

    fixes some bugs

--- src/Store/strore.js
+++ src/Store/strore.js
@@ -1,15 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import app from './application/reducer'
 import thunk from 'redux-thunk'
-import reducer from './reducers'
-import { getAllProducts } from './actions'
-import App from './containers/App'
 
 const middleware = [thunk];
-
-if (process.env.NODE_ENV !== 'production') {
-    middleware.push(createLogger());
-}
 
 export default createStore(
     app,
